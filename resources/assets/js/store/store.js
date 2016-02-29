2a2155d38db61fc5cff009370d42951812f52405
Jacob Michelsen <chromegadget@gmail.com>
2016-02-29 17:26:38 +0100

    Updated store syntax and added missing dependency

--- resources/assets/js/store/store.js
+++ resources/assets/js/store/store.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
-import Revue from 'revue'
-import {createStore} from 'redux'
+import Vue from 'vue';
+import Revue from 'revue';
+import {createStore} from 'redux';
 // create the logic how you would update the genomes
-import genomes from '../reducers/genomes'
+import genomes from '../reducers/genomes';
 // create some redux actions
-import actions from '../actions'
+import actions from '../actions/genomes';
 
 // create a redux store
-const reduxStore = createStore(genomes)
+const reduxStore = createStore(genomes);
 // binding the store to Vue instance, actions are optional
-const store = new Revue(Vue, reduxStore, actions)
+const store = new Revue(Vue, reduxStore, actions);
 // expose the store for your component to use
-export default store
+export default store;
